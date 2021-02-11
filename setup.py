6971de8e89b150b6cf8c41b0e82655be54795e3f
Proyecto <mena.jhon@correounivalle.edu.co>
2021-02-11 10:57:58 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="django-attributesjsonfield",
     url="https://github.com/JhonFrederick/django-attributesjsonfield",
     download_url="https://github.com/JhonFrederick/django-attributesjsonfield",
     license="MIT",
     author="",
     author_email="",
-    keywords='Django JsonField AttributesJSONField',
+    keywords="Django JsonField AttributesJSONField",
     description="Additional features for Django JSONField",
     classifiers=[
         "Programming Language :: Python",
@@ -18,5 +18,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["django>=2.2"]
+    packages=setuptools.find_packages(),
+    install_requires=["django>=2.2"],
 )
